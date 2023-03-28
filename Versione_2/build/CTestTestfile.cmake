d7492321f5d2220b49054888baa21f5601fb30a6
Francesco-Nasoni <f.nasoni34@gmail.com>
2023-03-28 14:12:49 +0200

    build dir remuved

--- Versione_2/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aaa/pandemic/Versione_2
-# Build directory: /home/aaa/pandemic/Versione_2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(epidemic.t "/home/aaa/pandemic/Versione_2/build/epidemic.t")
-set_tests_properties(epidemic.t PROPERTIES  _BACKTRACE_TRIPLES "/home/aaa/pandemic/Versione_2/CMakeLists.txt;25;add_test;/home/aaa/pandemic/Versione_2/CMakeLists.txt;0;")
