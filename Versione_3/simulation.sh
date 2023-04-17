ddc3bc4d95831c8133412af8c428e8e1d55db367
Francesco-Nasoni <f.nasoni34@gmail.com>
2023-04-17 22:04:09 +0200

    Little changes

--- Versione_3/simulation.sh
+++
@@ -1,6 +0,0 @@
-g++ main.cpp person.cpp pandemic1.cpp pandemicCM1.cpp graph.cpp utilities.cpp axis.cpp -O3 -Wall -Wextra -lsfml-graphics -lsfml-window -lsfml-system 
-#g++ aRoot.cpp -o analysis.out `root-config --cflags --glibs`
-#g++ aRootCM.cpp -o analysisCM.out `root-config --cflags --glibs`
-./a.out
-#./analysis.out
-#./analysisCM.out
--- Versione_3/test.sh
+++
@@ -1,2 +0,0 @@
-g++ pandemic.test.cpp pandemic1.cpp person.cpp graph.cpp axis.cpp utilities.cpp -O2 -lsfml-graphics -lsfml-window -lsfml-system -o test.out
-
